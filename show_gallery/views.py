44c78eda3b502c707edeea27371b630e8b217a33
Andres <andres.iwan@gmail.com>
2016-02-18 11:12:40 +0200

    Bug: Username did not match the actual user: fixed

--- show_gallery/views.py
+++ show_gallery/views.py
@@ -19,21 +19,24 @@ class Check_UserView(View):
         username = request.POST.get('username')
         # Check if username is empty
         if (username != ''):
+            # Look for username in the database
             # Filter by username
             user = User.objects.filter(user_name=username)
             # If username exists in the database return its account_id
             if (user.count()>0):
                 return redirect('show_gallery:template1', pk=user.values('account_id')[0]['account_id'])
             else:
-                r = requests.get('https://api.instagram.com/v1/users/search?access_token=1497402817.1fb234f.1b8969bb3b304945a6782ae574069017&q=%s' % username)
+                r = requests.get('https://api.instagram.com/v1/users/search?access_token=1497402817.1fb234f.1b8969bb3b304945a6782ae574069017&q={}' .format(username))
                 # Check if the API request returns any data, meaning that the user exists in instagram
                 data = r.json()['data']
                 if (data != []):
                     # Add user to database
-                    username = data[0]['username']
-                    id = data[0]['id']
+                    user = getUser(data, username)
+                    username = user['username']
+                    id = user['id']
                     new_user = User(account_id='{}'.format(id), user_name='{}' .format(username))
                     new_user.save()
+                    # Get the pictures
                     r = requests.get('https://api.instagram.com/v1/users/{}/media/recent?access_token=1497402817.1fb234f.1b8969bb3b304945a6782ae574069017' .format(id))
                     r = r.json()
                     for picture in r['data']:
@@ -67,3 +70,9 @@ class Template4View(TemplatesView):
     url = 'show_gallery/template4.html'
 class Template5View(TemplatesView):
     url = 'show_gallery/template5.html'
+
+# data is a list that contains all the users
+def getUser(data, username_entered_by_user):
+    for user in data:
+        if user['username'] == username_entered_by_user:
+            return user
